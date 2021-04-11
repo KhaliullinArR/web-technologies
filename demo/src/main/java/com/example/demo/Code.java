9bddda26f766c2b44937e2d5ec69436baa6a523a
KhaliullinArR <74345012+KhaliullinArR@users.noreply.github.com>
2021-04-12 00:36:49 +0300

    Project uploading

---
+++ demo/src/main/java/com/example/demo/Code.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+public class Code {
+    private    String code;
+
+    public Code(String code) {
+        this.code = code;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+}
